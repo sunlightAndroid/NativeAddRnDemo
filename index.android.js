89ec51b088f62623b28ee8082914c2d16387dc67
gechuanguang <1944633835@qq.com>
2017-03-14 15:42:57 +0800

    添加RN使用原生UI组件模块

--- index.android.js
+++ index.android.js
@@ -13,7 +13,7 @@ import {
     View
 } from 'react-native';
 
-
+import PaintView from './NativePaintView';
 export default class RNComponent extends Component {
 
     // 构造
@@ -25,54 +25,59 @@ export default class RNComponent extends Component {
         };
     }
 
-     render() {
-             tryCall = () => {
-                 var rnToastAndroid = NativeModules.MyNativeModule;
-                 rnToastAndroid.tryCallBack("luo", "131", (errorCallback) => {
-                         alert(errorCallback)
-                     },
-                     (successCallback) => {
-                         alert(successCallback);
-                     });
-             };
-             androidback = () => {
+    render() {
+        tryCall = () => {
+            var rnToastAndroid = NativeModules.MyNativeModule;
+            rnToastAndroid.tryCallBack("luo", "131", (errorCallback) => {
+                    alert(errorCallback)
+                },
+                (successCallback) => {
+                    alert(successCallback);
+                });
+        };
+        androidback = () => {
 
-                 var ANdroidNative = NativeModules.MyNativeModule;
-                 ANdroidNative.renderAndroidData((Callback) => {
-                     alert(Callback);
+            var ANdroidNative = NativeModules.MyNativeModule;
+            ANdroidNative.renderAndroidData((Callback) => {
+                alert(Callback);
 
-                 });
-             };
+            });
+        };
 
-             return (
+        return (
 
-                 <View style={styles.container}>
-                     <Text style={styles.welcome}
-                           onPress={this.call_button.bind(this)}
-                     >
-                         React Native 调用原生方法!
-                     </Text>
-                     <Text style={styles.instructions}
-                           onPress={()=>androidback()}
-                     >
-                         获得android回调的数据
-                     </Text>
-                     <Text style={styles.instructions}>
+            <View style={styles.container}>
 
-                         {NativeModules.MyNativeModule.rnCallNative(this.state.title)}
-                     </Text>
+                <PaintView style={styles.paintview}
+                           color={"#FF5984"}
+                           raduis={100}
+                />
+                <Text style={styles.welcome}
+                      onPress={this.call_button.bind(this)}
+                >
+                    React Native 调用原生方法!
+                </Text>
+                <Text style={styles.instructions}
+                      onPress={() => androidback()}
+                >
+                    获得android回调的数据
+                </Text>
+                <Text style={styles.instructions}>
 
-                     <Text style={styles.instructions} onPress={()=>tryCall()}>
-                         trycallAndroid
-                     </Text>
-                 </View>
-             );
-         }
+                    {NativeModules.MyNativeModule.rnCallNative(this.state.title)}
+                </Text>
 
-         call_button() {
+                <Text style={styles.instructions} onPress={() => tryCall()}>
+                    trycallAndroid
+                </Text>
+            </View>
+        );
+    }
 
-             NativeModules.MyNativeModule.rnCallNative('调用原生方法的Demo');
-         }
+    call_button() {
+
+        NativeModules.MyNativeModule.rnCallNative('调用原生方法的Demo');
+    }
 
 }
 const styles = StyleSheet.create({
@@ -80,7 +85,7 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: '#F5FCFF',
+        backgroundColor: '#fff9c0',
     },
     welcome: {
         fontSize: 20,
@@ -91,6 +96,11 @@ const styles = StyleSheet.create({
         textAlign: 'center',
         color: '#333333',
         marginBottom: 5,
+    },
+    paintview: {
+        width: 100,
+        height: 100,
+        backgroundColor: '#000'
     },
 });
 
